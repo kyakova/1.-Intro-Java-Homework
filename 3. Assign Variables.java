eb85475ac647abf3647766beb82f2b434506136d
kyakova <kyakova@yahoo.co.uk>
2016-03-25 11:05:54 +0200

    3. Assign Variables

---
+++ 3. Assign Variables.java
@@ -0,0 +1,18 @@
+package org.softuni;
+
+public class Main {
+
+    public static void main(String[] args) {
+	    byte firstByte = 127;
+        short firstShort = 32767;
+        int firstInt = 2000000000;
+        long firstLong = 919827112351L;
+        char firstChar = 'c';
+        boolean firstBool = false;
+        float firstFloat = 0.5f;
+        double firstDouble = 0.1234567891011;
+        String firstString = "Palo Alto";
+
+        System.out.println (firstByte + " " + firstShort + " " + firstInt  + " " + firstLong + " " + firstChar + " " + firstBool + " " + firstFloat + " " + firstDouble + " " + firstString);
+    }
+}
