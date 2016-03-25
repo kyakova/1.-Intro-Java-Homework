a81f36e3146e5e353a3bca573c4d847695f98810
kyakova <kyakova@yahoo.co.uk>
2016-03-25 11:07:00 +0200

    4. PrintCharacters

---
+++ 4. PrintCharacters.java
@@ -0,0 +1,12 @@
+package org.softuni;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        char ch;
+        for (ch = 'a'; ch <= 'z'; ch++) {
+            System.out.print(ch+ " ");
+        }
+    }
+}
